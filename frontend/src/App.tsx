5cb5c9e87abc3910a48404070dbd3836de165590
Geanderson Kassio <geankassiosilva@gmail.com>
2021-09-08 16:52:24 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div className="text-primary">
-      <h1>Olá mundo!</h1> 
-    </div>
+    <>
+      <NavBar />
+      <div className="text-primary container">
+        <h1>Olá mundo!</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/acenelio" target="_blank" rel="noreferrer">Geanderson Kássio</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/geandersonkassio" target="_blank" rel="noreferrer">@geandersonkassio</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
