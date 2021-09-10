f7a396c9c301cd914fd8fffede24116eb404635d
Geanderson Kassio <geankassiosilva@gmail.com>
2021-09-10 11:01:00 -0300

    Routes

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,20 @@
+import Dashboard from 'Pages/Dashboard';
+import Home from 'Pages/Home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+                <Route path="/dashboard">
+                    <Dashboard />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
