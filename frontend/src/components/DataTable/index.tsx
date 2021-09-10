904b4ee74bf5cbd2dfacad5158d82a34fd973049
Geanderson Kassio <geankassiosilva@gmail.com>
2021-09-10 17:42:05 -0300

    Env var

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -17,7 +17,7 @@ const DataTable = () => {
     });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales?page=${activePage}&size=5&sort=date,desc`)
+        axios.get(`${BASE_URL}/sales?page=${activePage}&size=10&sort=date,desc`)
             .then(response => {
                 setPage(response.data);
             })
