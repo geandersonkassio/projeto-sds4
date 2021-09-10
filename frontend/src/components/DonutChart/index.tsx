f3cb2b35f753fa34afc06bf965d6340c982282bb
Geanderson Kassio <geankassiosilva@gmail.com>
2021-09-10 15:49:54 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -10,17 +11,18 @@ type ChartData = {
 
 const DonutChart = () => {
 
-    let chartData: ChartData = { labels: [], series: [] };
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then((response) => {
-            const data = response.data as SaleSum[];
-            const labels = data.map(x => x.sellerName);
-            const series = data.map(x => x.sum);
-            chartData = { labels, series }
-            
-            console.log(chartData);
-        })
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then((response) => {
+                const data = response.data as SaleSum[];
+                const labels = data.map(x => x.sellerName);
+                const series = data.map(x => x.sum);
+
+                setChartData({ labels, series });
+            });
+    }, []);
 
     //const mockData = {
     //     series: [477138, 499928, 444867, 220426, 473088],
